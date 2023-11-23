140aee017009b4bb5ffbdd820600241ecdf08351
karolceballos <karol_ceballos13@hotmail.com>
2023-11-22 21:51:48 -0500

    correrprograma

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
 import express from 'express';
+import pg from 'pg'
+/*leer variable de entorno*/
+import { config } from 'dotenv';
+
+/* leer variables de entorno */
+config()
+
 
 const app = express();
+const pool=new pg.Pool({
+       connectionString:process.env.DATABASE_URL,/* ENV PARA LEER VARIABLES DE ENTORNO*/ 
+      /* ssl: true*/
+}
 
+
+)
+
+app.get('/ping', async (req, res) => {
+    const result = await pool.query('SELECT * FROM restaurantes');
+    return res.json(result.rows);
+});
 /* Ruta inicial */
 app.get('/', (req, res) => {
     res.send('hello world');
